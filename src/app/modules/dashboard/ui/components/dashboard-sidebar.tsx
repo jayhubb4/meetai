6d999241cc75d08aad514c39e39bda8a23cddc1f
Jonathan Hubbard <gsusplaya@gmail.com>
2025-09-25 00:33:42 -0400

    Finished sidebar

--- src/app/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/app/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -17,6 +17,7 @@ import { BotIcon, StarIcon, VideoIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { DashboardUserButton } from './dashboard-user-button';
 
 const firstSection = [
   {
@@ -108,6 +109,9 @@ export const DashboardSidebar = () => {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter className='text-white'>
+        <DashboardUserButton />
+      </SidebarFooter>
     </Sidebar>
   )
 }
